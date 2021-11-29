9773699f0b3b390e22c85a92e51e6a05bcec8977
renhubgit <renik2001@gmail.com>
2021-11-29 18:18:19 +0100

    img fix

--- src/home.js
+++ src/home.js
@@ -7,6 +7,7 @@ function renderHome() {
     home.innerHTML = `
         <div>
             <h3>Welcome to the "Mamma Mia" Pizzeria!</h3>
+            <img src="../dist/img/home.jpg">   
             <p>Here we serve all of our food with love! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate sed molestias nisi assumenda atque!<br><br> Nulla soluta aliquam aliquid ut vero, reiciendis eligendi nihil accusantium delectus, facilis quo illum! Repellendus, sapiente alias minima nemo molestias perferendis consequuntur autem fugiat facere facilis distinctio totam modi nisi aut corporis amet minus magni beatae aliquam eveniet, in cumque earum. </p>
         </div>            
     `
