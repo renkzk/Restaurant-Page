cf22fb2ddf0230639db1a611a57e2eb621f26bbf
renhubgit <renik2001@gmail.com>
2021-12-07 18:07:39 +0100

    fixed text overflow at higher resolutions

--- src/home.js
+++ src/home.js
@@ -5,11 +5,13 @@ function renderHome() {
     let home = document.createElement('main')
     home.classList.add('home')
     home.innerHTML = `
-        <div>
+    <div class="home-content">
+        <div class="home-text">
             <h3>Welcome to the "Mamma Mia" Pizzeria!</h3>
-            <img src="./img/home.jpg">   
             <p>Here we serve all of our food with love! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate sed molestias nisi assumenda atque!<br><br> Nulla soluta aliquam aliquid ut vero, reiciendis eligendi nihil accusantium delectus, facilis quo illum! Repellendus, sapiente alias minima nemo molestias perferendis consequuntur autem fugiat facere facilis distinctio totam modi nisi aut corporis amet minus magni beatae aliquam eveniet, in cumque earum. </p>
-        </div>            
+        </div>
+            <img src="./img/home.jpg">
+    </div>
     `
     contentContainer.append(home)
 
