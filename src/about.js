42171af71f583db149bcdf58f2660f555bc68a19
renhubgit <renik2001@gmail.com>
2022-10-15 21:08:39 +0200

    responsive styling

--- src/about.js
+++ src/about.js
@@ -1,23 +1,23 @@
 function renderAbout() {
-    let contentContainer = document.querySelector('#content')
-    contentContainer.innerHTML = ''
+  let contentContainer = document.querySelector("#content")
+  contentContainer.innerHTML = ""
 
-    let about = document.createElement('main')
-    about.classList.add('about')
-    about.innerHTML = `
+  let about = document.createElement("main")
+  about.classList.add("about")
+  about.innerHTML = `
         <div>
             <div class="about-info">
                 <p>📞 123 456 7890</p>
                 <p>📍 Not a Fake Adress 123, Naples, Italy</p>
             </div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23463.281798000255!2d14.23880907263062!3d40.83123834997451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x133b09ac26357211%3A0x970c840bd5800ad!2sCastel%20dell&#39;Ovo!5e0!3m2!1sit!2sit!4v1638139059907!5m2!1sit!2sit" width="800" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <iframe class='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23463.281798000255!2d14.23880907263062!3d40.83123834997451!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x133b09ac26357211%3A0x970c840bd5800ad!2sCastel%20dell&#39;Ovo!5e0!3m2!1sit!2sit!4v1638139059907!5m2!1sit!2sit" width="800" height="500" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     `
-    contentContainer.append(about)
+  contentContainer.append(about)
 
-    document.querySelector('#aboutBtn').classList.add('selected-tab')
-    document.querySelector('#homeBtn').classList.remove('selected-tab')
-    document.querySelector('#menuBtn').classList.remove('selected-tab')
+  document.querySelector("#aboutBtn").classList.add("selected-tab")
+  document.querySelector("#homeBtn").classList.remove("selected-tab")
+  document.querySelector("#menuBtn").classList.remove("selected-tab")
 }
 
-export {renderAbout}
+export { renderAbout }
--- src/home.js
+++ src/home.js
@@ -1,23 +1,23 @@
 function renderHome() {
-    let contentContainer = document.querySelector('#content')
-    contentContainer.innerHTML = ''
+  let contentContainer = document.querySelector("#content")
+  contentContainer.innerHTML = ""
 
-    let home = document.createElement('main')
-    home.classList.add('home')
-    home.innerHTML = `
+  let home = document.createElement("main")
+  home.classList.add("home")
+  home.innerHTML = `
     <div class="home-content">
         <div class="home-text">
             <h3>Welcome to the "Mamma Mia" Pizzeria!</h3>
-            <p>Here we serve all of our food with love! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate sed molestias nisi assumenda atque!<br><br> Nulla soluta aliquam aliquid ut vero, reiciendis eligendi nihil accusantium delectus, facilis quo illum! Repellendus, sapiente alias minima nemo molestias perferendis consequuntur autem fugiat facere facilis distinctio totam modi nisi aut corporis amet minus magni beatae aliquam eveniet, in cumque earum. </p>
+            <p>Here we serve all of our food with love! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate sed molestias nisi assumenda atque!<br><br> Nulla soluta aliquam aliquid ut vero, reiciendis eligendi nihil accusantium delectus, facilis quo illum! Repellendus, sapiente alias minima nemo molestias perferendis consequuntur autem.</p>
         </div>
             <img src="./img/home.jpg">
     </div>
     `
-    contentContainer.append(home)
+  contentContainer.append(home)
 
-    document.querySelector('#homeBtn').classList.add('selected-tab')
-    document.querySelector('#menuBtn').classList.remove('selected-tab')
-    document.querySelector('#aboutBtn').classList.remove('selected-tab')
+  document.querySelector("#homeBtn").classList.add("selected-tab")
+  document.querySelector("#menuBtn").classList.remove("selected-tab")
+  document.querySelector("#aboutBtn").classList.remove("selected-tab")
 }
 
-export {renderHome}
+export { renderHome }
